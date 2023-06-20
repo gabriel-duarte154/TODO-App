870653fd66722376437cb58590f682520b864615
gabriel-duarte154 <127501966+gabriel-duarte154@users.noreply.github.com>
2023-06-20 16:40:20 -0300

    Set up archives and creating html template

---
+++ main.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('it\'s up to start')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxnQyIsInNvdXJjZXMiOlsid2VicGFjazovL3RvZG8tYXBwLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnNvbGUubG9nKCdpdFxcJ3MgdXAgdG8gc3RhcnQnKSJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
